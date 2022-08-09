858a0550e92d9800578ba404af6b4bc85fb3b939
Ed Beroset <beroset@ieee.org>
2022-08-09 11:03:38 -0400

    Add starString helper
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- templates/_helpers/watchmodels.js
+++ templates/_helpers/watchmodels.js
@@ -19,4 +19,32 @@ module.exports.register = function (Handlebars, options, params) {
   Handlebars.registerHelper('getModel', function(codename) {
    return models.watches.find((s) => s.name===codename);
   });
+
+  /**
+   * Create stars string from passed value
+   *
+   * @param   {Number}  stars    Number of stars
+   * @param   {Number}  maxstars Maximum number of stars
+   * @return  {String}           Stars string
+   */
+
+  Handlebars.registerHelper('starString', function(stars, maxstars) {
+      stars = parseInt(stars);
+      maxstars = parseInt(maxstars);
+      if (stars < 0 || maxstars < 0 || maxstars > 10 || stars > maxstars) {
+          return "";
+      }
+      var ret='<span class="star-good">';
+      for (let i = 0; i < stars; i++) {
+          ret += '<i class="icon ion-md-star"></i>';
+      }
+      ret += '</span><span class="star-bad">';
+      for (let i = stars; i < maxstars; i++) {
+          ret += '<i class="icon ion-md-star"></i>';
+      }
+      ret += '</span>';
+      return ret;
+      
+  });
+
 };
